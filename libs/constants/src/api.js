69f862fab9190a1c76e2b4402e9119229820232c
RoyerM390 <rmendezr2@miumg.edu.gt>
2024-09-20 17:26:27 -0600

    apuntando a url de produccion

--- libs/constants/src/api.js
+++ libs/constants/src/api.js
@@ -1,5 +1,5 @@
-// export const url = 'https://api.mspas.tuventa.net/api';
-export const url = 'http://localhost:5500/api';
+export const url = 'https://api.mspas.lat/api';
+// export const url = 'http://localhost:5500/api';
 export const googleMapsKey = 'AIzaSyBRFILbxy45s95AvTev5Ean5_8m73jtJIQ';
 export const googleMapsApi = `https://www.googleapis.com/geolocation/v1/geolocate?key=${googleMapsKey}`;
 // export const url = 'https://api.accel.tuventa.net/api';
